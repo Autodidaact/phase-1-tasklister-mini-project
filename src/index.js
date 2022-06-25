adcf993237950d6ce5d80c7d258aa4a925aba102
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-25 22:50:12 +0000

    fixed the tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    todoItem(e.target.new_task_description.value);
+    form.reset();
+  })
+
+  
 });
+function todoItem(item){
+  let p = document.createElement('p');
+  let button = document.createElement('button');
+  button.addEventListener('click', deleteItem)
+  button.textContent = 'x';
+  p.textContent = `${item}`;
+  p.appendChild(button)
+  
+  document.getElementById('list').appendChild(p);
+}
+function deleteItem(e){
+  e.target.parentNode.remove();
+}
